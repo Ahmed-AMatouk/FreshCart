13aadc48375f22460d41db0977da954efc2e9eee
Ahmed Ali <amatouk632@gmail.com>
2026-05-06 02:43:25 +0300

    solve getMyToken

--- utils/getMyToken.ts
+++ utils/getMyToken.ts
@@ -1,7 +1,7 @@
 import { decode, JWT } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export async function getMyToken() :Promise<JWT>{
+export async function getMyToken() :Promise<JWT|undefined>{
     const mycookies = await cookies();
     const myTokenCookies = mycookies.get("next-auth.session-token")?.value;
     const myTokenAfterDecoded = await decode({token:myTokenCookies , secret: process.env.BETTER_AUTH_SECRET!});
