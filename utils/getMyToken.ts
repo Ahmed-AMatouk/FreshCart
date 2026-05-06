0acaf22fb6c1f503e4c0e8d500eddaab41371487
Ahmed Ali <amatouk632@gmail.com>
2026-05-06 03:42:30 +0300

    Update cookies name in production

--- utils/getMyToken.ts
+++ utils/getMyToken.ts
@@ -3,7 +3,8 @@ import { cookies } from "next/headers";
 
 export async function getMyToken() :Promise<JWT|undefined>{
     const mycookies = await cookies();
-    const myTokenCookies = mycookies.get("next-auth.session-token")?.value;
+    const myTokenCookies = mycookies.get("__Secure-next-auth.session-token")?.value || 
+    mycookies.get("next-auth.session-token")?.value;
     const myTokenAfterDecoded = await decode({token:myTokenCookies , secret: process.env.BETTER_AUTH_SECRET!});
     if(myTokenAfterDecoded)return myTokenAfterDecoded;
 }
